75bae02148883d55f7a7d3fd045eb50ab8b93832
KrolKamil <kamilkrol1997@wp.pl>
2020-03-21 13:40:12 +0100

    joi schema

--- services/validators/schemas/index.js
+++ services/validators/schemas/index.js
@@ -1,4 +1,22 @@
-const Joi = require('joi');
-const addTaskSchemaWithoutGenres = Joi.object({
-
+const Joi = require('@hapi/joi');
+const addFilm = Joi.object({
+  genres: Joi.array(),
+  title: Joi.string().min(1).max(30).required(),
+  year: Joi.number().required(),
+  runtime: Joi.number().required(),
+  director: Joi.string().max(255).required(),
+  actors: Joi.string().optional(),
+  plot: Joi.string().optional(),
+  posterUrl: Joi.string().optional()
 });
+
+exports.addFilm = addFilm;
+
+// - a list of genres (only predefined ones from db file) (required, array of predefined strings)
+// - title (required, string, max 255 characters)
+// - year (required, number)
+// - runtime (required, number)
+// - director (required, string, max 255 characters)
+// - actors (optional, string)
+// - plot (optional, string)
+// - posterUrl (optional, string)
