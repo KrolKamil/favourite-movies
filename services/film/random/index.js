a1776069327ea3c8b7fcc12e9b8eba75cb4323ca
KrolKamil <kamilkrol1997@wp.pl>
2020-03-22 22:23:42 +0100

    before testing

--- services/film/random/index.js
+++ services/film/random/index.js
@@ -1,14 +1,14 @@
 const utils = require('../../../utils');
 
-const getRandomFilm = (filmsDB, payload) => {
+const getRandomFilm = (films, payload) => {
   if (payload.genres && payload.duration) {
-
+    return getRandomFilmByGenres(films, payload.genres);
   } else if (payload.genres) {
-
+    return getRandomFilmByGenres(films, payload.genres);
   } else if (payload.duration) {
-    getRandomFilmByDurration(filmsDB, payload.duration);
+    return getRandomFilmByDurration(films, payload.duration);
   } else {
-    getSingleRandomFilm(filmsDB.movies);
+    return getSingleRandomFilm(films);
   }
 };
 
@@ -28,8 +28,38 @@ const getRandomFilmByDurration = (films, durration) => {
   return getSingleRandomFilm(filmsWithSpecifiedDurration);
 };
 
-const getRandomFilmByGenres = () => {
+const getRandomFilmByGenres = (films, genres) => {
+  const rootArray = createArrayOfArrays(genres.length);
+  let i = 0;
 
+  let filmsLength = films.length;
+  for (i; i < filmsLength; i++) {
+    const countFilmValidGenres = countValidGenresInFilmGenres(genres, films[i].genres);
+    if (countFilmValidGenres > 0) {
+      rootArray[countFilmValidGenres - 1].push(films[i]);
+    }
+  }
+  return rootArray.flat();
+};
+
+const countValidGenresInFilmGenres = (validGenres, filmGenres) => {
+  let validGenresInFilmGenres = 0;
+  let i = 0;
+  for (i; i < filmGenres.length; i++) {
+    if (validGenres.indexOf(filmGenres[i]) >= 0) {
+      validGenresInFilmGenres++;
+    }
+  }
+  return validGenresInFilmGenres;
+};
+
+const createArrayOfArrays = (length) => {
+  const rootArray = [];
+  let i = 0;
+  for (i; i < length; i++) {
+    rootArray.push([]);
+  }
+  return rootArray;
 };
 
 module.exports = {
